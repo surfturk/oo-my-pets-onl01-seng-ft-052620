18ba306dbfd078c69abdb65f888db031c80308bf
Berkley Brown <berkleydeane@gmail.com>
2020-06-11 14:10:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,65 +1,57 @@
 class Owner
-  
-  attr_accessor , :pets
-  attr_reader :name
-
-@@owners = []
-
+  # code goes here
+  attr_accessor
+  attr_writer
+  attr_reader :name, :species
+  @@owner = []
   def initialize(name)
     @name = name
-    @species = "human"
-    @@owners << self
-    @pets = {dogs: [], cats: []}
+    @species = “human”
+    @@owner << self
   end
-
-  def self.all
-    @@owners
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def say_species
-    "I am a #{@species}."
+    “I am a #{@species}.”
   end
-
-  def buy_cat(cat_name)
-    pets[:cats] << Cat.new(cat_name)
+  def self.all()
+    @@owner
   end
-
-  def buy_dog(dog_name)
-    pets[:dogs] << Dog.new(dog_name)
+  def self.count
+    @@owner.size
   end
-
+  def self.reset_all
+    @@owner.clear
+  end
+  def cats
+    owned_cat = Cat.all.select {|cat| cat.owner == self}
+    owned_cat
+  end
+  def dogs
+    owned_dog = Dog.all.select {|dog| dog.owner == self}
+    owned_dog
+  end
+  def buy_cat(cat)
+    Cat.new(cat, self)
+  end
+  def buy_dog(dog)
+    Dog.new(dog, self)
+  end
   def walk_dogs
-    pets[:dogs].each do |dog|
-      dog.mood = "happy"
-    end
+    self.dogs.each {|dog| dog.mood = “happy”}
   end
-
-  def play_with_cats
-    pets[:cats].each do |cat|
-      cat.mood = "happy"
-    end
+    def feed_cats
+    self.cats.each {|cat| cat.mood = “happy”}
   end
-
- 
-
   def sell_pets
-    pets.each do |species, animals|
-      animals.each do |animal|
-        animal.mood = "nervous"
-        end
-      animals.clear
-    end
+     self.dogs.each do |dogs|
+       dogs.mood = “nervous”
+       dogs.owner = nil
+     end
+    self.cats.each do |cats|
+      cats.mood = “nervous”
+      cats.owner = nil
   end
-
   def list_pets
-    "I have #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
-  end
+   “I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s).”
+ end
+end
 end
