fecd3a5233690d189db88aeca51d151090c0c5cf
Berkley Brown <berkleydeane@gmail.com>
2020-06-11 13:58:56 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+ 
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+  end
+
+  def mood
+    @mood
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,65 @@
 class Owner
-  # code goes here
+  
+  attr_accessor , :pets
+  attr_reader :name
+
+@@owners = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@owners << self
+    @pets = {dogs: [], cats: []}
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_cat(cat_name)
+    pets[:cats] << Cat.new(cat_name)
+  end
+
+  def buy_dog(dog_name)
+    pets[:dogs] << Dog.new(dog_name)
+  end
+
+  def walk_dogs
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+ 
+
+  def sell_pets
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+        end
+      animals.clear
+    end
+  end
+
+  def list_pets
+    "I have #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  end
 end
